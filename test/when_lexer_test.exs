03125643742c97db8fc5239b1ca8ceb489781a82
Damjan Becirovic <damjan.becirovic@gmail.com>
2019-04-10 22:51:51 +0200

    Implemented leex tokenizer

---
+++ test/when_lexer_test.exs
@@ -0,0 +1,58 @@
+defmodule When.WhenLexer.Test do
+  use ExUnit.Case
+
+  @test_examples [
+    "true",
+    "'true'",
+    "('false')",
+    "(FALSE) AND TAG != 'v1.*'",
+    "branch = 'master' and tag =~ 'v1.*'",
+    "(branch = 'master' and tag =~ 'v1.*') or result != 'passed'",
+    "(branch = 'master' AND tag =~ 'v1.*') OR result_reason != 'stopped'",
+    "((BRANCH !~ 'master') and tag =~ 'v1.*') OR (result_reason != 'stopped')",
+  ]
+
+  @expected_example_results [
+    [{:boolean, 1, "true"}],
+    [{:string, 1, "true"}],
+    [{:'(',  1}, {:string, 1, "false"}, {:')',  1}],
+    [
+     {:'(',  1}, {:boolean, 1, "false"}, {:')',  1}, {:bool_operator, 1, "and"},
+     {:keyword, 1, "tag"}, {:operator, 1, "!="}, {:string, 1, "v1.*"}
+    ],
+    [
+     {:keyword, 1, "branch"}, {:operator, 1, "="}, {:string, 1, "master"},
+     {:bool_operator, 1, "and"}, {:keyword, 1, "tag"}, {:operator, 1, "=~"},
+     {:string, 1, "v1.*"}
+    ],
+    [
+     {:'(',  1}, {:keyword, 1, "branch"}, {:operator, 1, "="}, {:string, 1, "master"},
+     {:bool_operator, 1, "and"}, {:keyword, 1, "tag"}, {:operator, 1, "=~"},
+     {:string, 1, "v1.*"}, {:')',  1}, {:bool_operator, 1, "or"}, {:keyword, 1, "result"},
+     {:operator, 1, "!="}, {:string, 1, "passed"}
+    ],
+    [
+     {:'(',  1}, {:keyword, 1, "branch"}, {:operator, 1, "="}, {:string, 1, "master"},
+     {:bool_operator, 1, "and"}, {:keyword, 1, "tag"}, {:operator, 1, "=~"},
+     {:string, 1, "v1.*"}, {:')',  1}, {:bool_operator, 1, "or"},
+     {:keyword, 1, "result_reason"}, {:operator, 1, "!="}, {:string, 1, "stopped"}
+    ],
+    [
+     {:'(',  1}, {:'(',  1}, {:keyword, 1, "branch"}, {:operator, 1, "!~"},
+     {:string, 1, "master"}, {:')',  1}, {:bool_operator, 1, "and"},
+     {:keyword, 1, "tag"}, {:operator, 1, "=~"}, {:string, 1, "v1.*"}, {:')',  1},
+     {:bool_operator, 1, "or"}, {:'(',  1}, {:keyword, 1, "result_reason"},
+     {:operator, 1, "!="}, {:string, 1, "stopped"}, {:')',  1}
+    ]
+  ]
+
+  test "test lexer behavior for various string examples" do
+    @test_examples
+    |> Enum.with_index()
+    |> Enum.map(fn {string, index} ->
+      assert {:ok, tokens, _} = string |> to_charlist() |> :when_lexer.string()
+      assert {tokens, index}
+         == {@expected_example_results |> Enum.at(index), index}
+    end)
+  end
+end
